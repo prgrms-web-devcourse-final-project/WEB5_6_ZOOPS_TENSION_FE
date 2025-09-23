5cb311b7e1ad1cab364e5497616ec403de1e01cd
Jaden <hyunbin.kim.3376@gmail.com>
2025-09-23 22:27:18 +0900

    [feat] 크롬 익스텐션 작업 중지 & 백엔드 로그인 구현 왼료 되면 다시 진행할 예정

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,8 +1,12 @@
 chrome.runtime.onMessage.addListener(async (message, sender) => {
-  if (message.action) {
+  if (message.action === 'openSidePanel') {
     const res = await openSideTab(sender.tab?.id);
 
     return res;
+  }
+
+  if (message.action === 'close-sidePanel') {
+    setTimeout(() => window.close(), 2000);
   }
 });
 
--- src/sidePanel/components/Button.tsx
+++ src/sidePanel/components/Button.tsx
@@ -7,6 +7,11 @@ interface Props {
 }
 
 const Button = ({ children, icon, className }: Props) => {
+  // 사이드 판넬 닫기 테스트
+  const handleClick = async () => {
+    await chrome.runtime.sendMessage({ action: 'close-sidePanel' });
+  };
+
   return (
     <button
       type="button"
@@ -14,6 +19,7 @@ const Button = ({ children, icon, className }: Props) => {
         'flex-center gap-1 text-black rounded-lg py-3 w-full cursor-pointer',
         className
       )}
+      onClick={handleClick}
     >
       {icon}
       <span className="font-bold text-base">{children}</span>
